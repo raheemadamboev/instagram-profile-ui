e37b08ade364055fc765bd364311ca006044d528
Raheem <raheemideadev@gmail.com>
2021-10-23 19:22:59 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/instagramprofileui/MainActivity.kt
@@ -0,0 +1,23 @@
+package xyz.teamgravity.instagramprofileui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.instagramprofileui.ui.theme.InstagramProfileUITheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstagramProfileUITheme {
+
+            }
+        }
+    }
+}
