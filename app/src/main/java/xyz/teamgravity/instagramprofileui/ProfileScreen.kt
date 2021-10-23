ee22b7655e8c1ca442b55f99c45f577797e5c050
Raheem <raheemideadev@gmail.com>
2021-10-23 21:07:36 +0500

    description section

--- app/src/main/java/xyz/teamgravity/instagramprofileui/ProfileScreen.kt
+++ app/src/main/java/xyz/teamgravity/instagramprofileui/ProfileScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -91,6 +93,14 @@ fun ProfileSection(
 
             StatsSection(modifier = Modifier.weight(7f))
         }
+
+        ProfileDescription(
+            displayName = "Raheem Adamboev",
+            description = "Android developer at EPAM systems\uD83D\uDCBB\nPassionate about life, money and adventure\uD83C\uDFD6\nUzbekistan\'s most wanted\uD83D\uDCAF",
+            url = "https://telegra.ph/Portfolio-of-Raheem-05-19",
+            followedBy = listOf("justinbieber", "miakhalifa"),
+            otherCount = 17
+        )
     }
 }
 
@@ -149,5 +159,69 @@ fun ProfileStat(
         Spacer(modifier = Modifier.height(4.dp))
 
         Text(text = text)
+    }
+}
+
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    description: String,
+    url: String,
+    followedBy: List<String>,
+    otherCount: Int
+) {
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = displayName,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+
+        Text(
+            text = description,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+
+        Text(
+            text = url,
+            color = Color(0xFF3D3D91),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+
+        if (followedBy.isNotEmpty()) {
+            Text(
+                text = buildAnnotatedString {
+                    val boldStyle = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+                    append("Followed by ")
+                    followedBy.forEachIndexed { index, name ->
+                        pushStyle(boldStyle)
+                        append(name)
+                        pop()
+                        if (index < followedBy.size - 1) {
+                            append(", ")
+                        }
+                    }
+                    if (otherCount > 2) {
+                        append(" and ")
+                        pushStyle(boldStyle)
+                        append("$otherCount others")
+                    }
+                },
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+        }
     }
 }
